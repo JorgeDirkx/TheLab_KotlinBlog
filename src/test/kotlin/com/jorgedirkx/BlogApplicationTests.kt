3c281b0487e96548bb551fac7ae7cf328ed69379
JorgeDirkx <jorge.dirkx@gmail.com>
2020-10-25 20:38:25 +0100

    Initial commit + testing

---
+++ src/test/kotlin/com/jorgedirkx/BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jorgedirkx
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
